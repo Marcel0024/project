1fc183dbdcc349869fe5679cda45c7cce9f49577
Marcel C <marcelcroes24@gmail.com>
2015-09-26 19:12:59 +0200

    heh

--- Leertaak1/src/Server.java
+++ Leertaak1/src/Server.java
@@ -1,18 +1,14 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 
 public class Server {
 	public static void main(String args[]){
 		try {
 	
-			ServerSocket sock = new ServerSocket(6052);
+			ServerSocket sock = new ServerSocket(7789);
 			Socket a = sock.accept();
 			Thread temp = new Thread(new Weather(a));
+			temp.start();
 			
 		} 
 		
@@ -20,9 +16,9 @@ public class Server {
 		
 		}
 }
+
 	
 	class Weather implements Runnable{
-
 		private Socket socket;
 		
 		public Weather(Socket socket){
@@ -32,20 +28,16 @@ public class Server {
 		@Override
 		public void run() {
 			try {
-				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
-				
+				BufferedReader in = new BufferedReader(
+						new InputStreamReader(socket.getInputStream()));
 				String line;
 				while((line = in.readLine()) != null){
 					
-				//data in sql here
-					
+					System.out.println(line);
+				
 				}
+				
 			} catch (IOException e) {}
-			
 		}
-		
-		
-		
 	}
 
