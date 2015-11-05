5da6690db69e2d97e14d7593bb8e6d02b4c98a32
Serkan <ser66orh_97@live.nl>
2015-11-05 11:44:00 +0100

    new vaadin project

---
+++ WeatherWebApp/src/com/example/weatherwebapp/WeatherwebappUI.java
@@ -0,0 +1,40 @@
+package com.example.weatherwebapp;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+@SuppressWarnings("serial")
+@Theme("weatherwebapp")
+public class WeatherwebappUI extends UI {
+
+	@WebServlet(value = "/*", asyncSupported = true)
+	@VaadinServletConfiguration(productionMode = false, ui = WeatherwebappUI.class)
+	public static class Servlet extends VaadinServlet {
+	}
+
+	@Override
+	protected void init(VaadinRequest request) {
+		setTheme("valo");
+		final VerticalLayout layout = new VerticalLayout();
+		layout.setMargin(true);
+		setContent(layout);
+
+		Button button = new Button("Click Me");
+		button.addClickListener(new Button.ClickListener() {
+			public void buttonClick(ClickEvent event) {
+				layout.addComponent(new Label("Thank you for clicking pussy"));
+			}
+		});
+		layout.addComponent(button);
+	}
+
+}
